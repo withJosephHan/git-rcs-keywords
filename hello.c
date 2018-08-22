209d6a4030a2f1f937f225cfd37cb6e6151e656e
HanJuyang <zy1256@grooo.co.jp>
2018-08-22 16:50:22 +0900

    22

--- hello.c
+++ hello.c
@@ -1,12 +1,1 @@
-/**
- * @file        hello.c
- * @author      @$Author$
- * @date	@$Date$
- * @version	@$Revision$
- *
- * $Id$
- */
-
-int main(int argc, char *argv[])
-  printf("Hello, world!112244");
-
+$Date$
